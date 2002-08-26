ec66e3e71b3fbd127e49eedf19f0ec6a2ae53759
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-08-26 06:15:10 +0000

    Add player list Expand class definition
    (Logical change 1.4)

--- koalamud/playerchar.hxx
+++ koalamud/playerchar.hxx
@@ -17,11 +17,15 @@
 
 #include <qobject.h>
 #include <qlistview.h>
+#include <qptrlist.h>
 
 #include "main.hxx"
 #include "char.hxx"
 #include "network.hxx"
 #include "koalastatus.h"
+
+class K_PlayerChar;
+#include "cmd.hxx"
 
 /**
  * Player Character object - Handles interaction that is specific to player characters.
@@ -30,16 +34,38 @@
 class K_PlayerChar : public virtual KoalaDescriptor, public virtual K_Char
 {
     Q_OBJECT
+
+		public:
+		typedef enum {STATE_GETNAME, STATE_PLAYING} state_t;
+
+		protected:
+		state_t _state;
     
     public:
     K_PlayerChar(int sock, QObject *parent=0, const char *name=0);
     virtual ~K_PlayerChar();
+		virtual void setName(QString name);
+		virtual void sendWelcome(void);
     
     private slots:
     virtual void readclient(void);
+
+		public slots:
+		virtual void setdisconnect(bool dis = true) { _disconnecting = dis; }
+		virtual void runcmd(cmdentry_t *cmd, QString word, QString arg);
     
     protected:
     QListViewItem *plrstatuslistitem;
+		bool _disconnecting;
 };
+
+typedef QPtrList<K_PlayerChar> playerlist_t;
+typedef QPtrListIterator<K_PlayerChar> playerlistiterator_t;
+
+#ifdef KOALA_PLAYERCHAR_CXX
+playerlist_t connectedplayerlist;
+#else
+extern playerlist_t connectedplayerlist;
+#endif
 
 #endif  //  KOALA_PLAYERCHAR_HXX

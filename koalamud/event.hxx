08fc6f01abe36d763d75c7431691ab4bad894b97
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-08-27 05:42:39 +0000

    (Logical change 1.5)

---
+++ koalamud/event.hxx
@@ -0,0 +1,23 @@
+/***************************************************************\
+*                        KoalaMud Gen 2                         *
+*    Copyright (c) 2002 First Step Internet Services, Inc.      *
+*                     All Rights Reserved                       *
+*        Distributed under the terms of the FSI License         *
+* See file LICENSE in the root of this package for information  *
+\***************************************************************/
+/***************************************************************\
+*	Module:
+*	Author:
+* Description:
+* Classes:
+\***************************************************************/
+
+#ifndef KOALA_EVENT_HXX
+#define KOALA_EVENT_HXX "%A%"
+
+#include <qevent.h>
+#include <qthread.h>
+
+#define EVENT_CHAR_OUTPUT		QEvent::User + 1
+
+#endif //  KOALA_EVENT_HXX

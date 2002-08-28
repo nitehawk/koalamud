53f04ea5923b2c104ce94beeca97ca7d0368b09e
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-08-28 21:14:40 +0000

    (Logical change 1.6)

---
+++ koalamud/comm.cpp
@@ -0,0 +1,131 @@
+/***************************************************************\
+*                        KoalaMud Gen 2                         *
+*    Copyright (c) 2002 First Step Internet Services, Inc.      *
+*                     All Rights Reserved                       *
+*        Distributed under the terms of the FSI License         *
+* See file LICENSE in the root of this package for information  *
+\***************************************************************/
+/***************************************************************\
+*	Module:
+*	Author:
+* Description:
+* Classes:
+\***************************************************************/
+
+#define KOALA_COMM_CXX "%A%"
+
+#include "comm.hxx"
+#include "playerchar.hxx"
+
+KOALACMD(cmd_tell)
+{
+	QString to = args.section(' ', 0, 0);
+	QString msg = args.section(' ', 1);
+
+	/* Check that we have parameters */
+	if (to.isEmpty())
+	{
+		QString str;
+		QTextOStream os(&str);
+		os << "Who would you like to tell something to?" << endl;
+		ch->sendtochar(str);
+		return;
+	} else if (msg.isEmpty())
+	{
+		QString str;
+		QTextOStream os(&str);
+		os << "What would you like to tell " << to << "?" << endl;
+		ch->sendtochar(str);
+		return;
+	}
+
+	/* First up we need to get a pointer to the player */
+	K_PlayerChar *tellto = connectedplayermap[to];
+	if (tellto == NULL)
+	{
+		QString str;
+		QTextOStream os(&str);
+		os << "That player is not logged on." << endl;
+		ch->sendtochar(str);
+		return;
+	} else if (ch == tellto)
+	{
+		QString str;
+		QTextOStream os(&str);
+		os << "You try to tell yourself something." << endl;
+		ch->sendtochar(str);
+		return;
+	}
+
+	/* Construct message to target */
+	QString tstr;
+	QTextOStream tos(&tstr);
+	tos << ch->getName(tellto) << " tells you, '" << msg << "'" << endl;
+	tellto->sendtochar(tstr);
+
+	/* Construct message to sender */
+	QString sstr;
+	QTextOStream sos(&sstr);
+	sos << "You tell " << tellto->getName(ch) << ", '" << msg << "'" << endl;
+	ch->sendtochar(sstr);
+}
+
+KOALACMD(cmd_gossip)
+{
+	KoalaChannel *goschan = channelmap["gossip"];
+	if (goschan == NULL)
+	{
+		return;
+	}
+
+	goschan->sendtochannel(ch, args);
+}
+
+/* Load communications commands into the dictionary */
+/* NOTE:  We also setup the comm channels here */
+void initcommcmddict(void)
+{
+	cmddict.insert(QString("tell"), new cmdentry_t("tell", cmd_tell));
+	cmddict.insert(QString("gossip"), new cmdentry_t("gossip", cmd_gossip));
+	cmddict.insert(QString("gos"), new cmdentry_t("gossip", cmd_gossip));
+
+	new KoalaChannel("gossip", "%sender% gossips, '%message%'",
+			"%sender% gossip, '%message%'");
+}
+
+/* Channel class implementation */
+KoalaChannel::KoalaChannel(QString name, QString chantemplateall,
+		QString chantemplatesender)
+	: QObject(NULL, name), _name(name), _templateall(chantemplateall),
+		_templatesender(chantemplatesender)
+{
+	/* Put channel into the channel map */
+	channelmap.insert(_name, this);
+}
+
+KoalaChannel::~KoalaChannel(void)
+{
+	channelmap.remove(_name);
+}
+
+void KoalaChannel::joinchannel(K_PlayerChar *ch)
+{
+	connect(this,
+		SIGNAL(channelmessagesent(K_PlayerChar *, QString, QString, QString)),
+		ch,
+		SLOT(channelsendtochar(K_PlayerChar *, QString, QString, QString)));
+		
+	connect(this, SIGNAL(channeldeleted(KoalaChannel *)),
+		ch, SLOT(channeldeleted(KoalaChannel *)));
+}
+
+void KoalaChannel::leavechannel(K_PlayerChar *ch)
+{
+	disconnect(ch);
+}
+
+void KoalaChannel::sendtochannel(K_PlayerChar *ch, QString msg)
+{
+	/* For the moment, just emit the signal, no processing */
+	emit channelmessagesent(ch, _templateall, _templatesender, msg);
+}
---
+++ koalamud/comm.hxx
@@ -0,0 +1,66 @@
+/***************************************************************\
+*                        KoalaMud Gen 2                         *
+*    Copyright (c) 2002 First Step Internet Services, Inc.      *
+*                     All Rights Reserved                       *
+*        Distributed under the terms of the FSI License         *
+* See file LICENSE in the root of this package for information  *
+\***************************************************************/
+/***************************************************************\
+*	Module:
+*	Author:
+* Description:
+* Classes:
+\***************************************************************/
+
+#ifndef KOALA_COMM_HXX
+#define KOALA_COMM_HXX "%A%"
+
+#include <qobject.h>
+#include <qdict.h>
+
+class KoalaChannel;
+#include "cmd.hxx"
+
+/* Channel class */
+class KoalaChannel : public QObject
+{
+	/* NOTE:  right now there is no way to generate a list of players in the
+	 * channel.  This may change if it looks necessary, but at this point, there
+	 * are no plans to use the channel system on its own to provide player
+	 * channels where member lists are useful */
+	Q_OBJECT
+
+	protected:
+		QString _name;
+		QString _templateall;
+		QString _templatesender;
+
+	public:
+		KoalaChannel(QString name, QString chantemplateall,
+				QString chantemplatesender);
+		~KoalaChannel();
+		void joinchannel(K_PlayerChar *ch);
+		void leavechannel(K_PlayerChar *ch);
+		QString getName(void) { return _name; }
+		void sendtochannel(K_PlayerChar *, QString msg);
+
+	signals:
+		/* When joinchannel is called, the player is connected to the
+		 * channelmessagesent and channeldeleted signals.  channeldeleted allows
+		 * us to ensure that we don't attempt to display dead channels if the
+		 * player requests a list of channels they are on */
+		void channelmessagesent(K_PlayerChar *sender, QString chantemplate,
+				QString chantemplatesender, QString message);
+		void channeldeleted(KoalaChannel *chan);
+};
+
+/* Prototypes */
+void initcommcmddict(void);
+
+#ifdef KOALA_COMM_CXX
+QDict<KoalaChannel> channelmap(101, false);
+#else
+extern QDict<KoalaChannel> channelmap;
+#endif // KOALA_COMM_CXX
+
+#endif //  KOALA_COMM_HXX

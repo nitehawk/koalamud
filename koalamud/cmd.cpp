f17932254b1b94ff1e3fc1ef4419393296ff2a17
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-08-28 21:14:39 +0000

    Move command dictionary initialize function to end of cmd.cpp do we don't need to keep a list of prototypes.
    (Logical change 1.6)

--- koalamud/cmd.cpp
+++ koalamud/cmd.cpp
@@ -15,28 +15,28 @@
 #define KOALA_CMD_CXX "%A%"
 
 #include "cmd.hxx"
-
-/* command prototypes - FIXME: Temp */
-KOALACMD(cmd_quit);
-KOALACMD(cmd_who);
-
-void initcmddict(void)
-{
-	cmddict.insert(QString("quit"), new cmdentry_t("quit", cmd_quit));
-	cmddict.insert(QString("who"), new cmdentry_t("who", cmd_who));
-}
+#include "comm.hxx"
 
 KOALACMD(cmd_quit)
 {
+	QString str;
+	QTextOStream os(&str);
+
+	os << "Thank you for playing on KoalaMud." << endl;
+	os << "Please return soon." << endl;
+	ch->sendtochar(str);
+
 	ch->setdisconnect(true);
 }
 
 KOALACMD(cmd_who)
 {
+	QString str;
+	QTextOStream os(&str);
+
 	/* Grab an iterator to the player list and loop through it */
 	playerlistiterator_t pli(connectedplayerlist);
 	K_PlayerChar *cur;
-	QTextStream os(ch);
 	os << "The following players are online: " << endl;
 	while ((cur = pli.current()) != NULL)
 	{
@@ -45,4 +45,13 @@ KOALACMD(cmd_who)
 		 * imm invisibility and a lot of other stuff before displaying someone */
 		os << cur->getName(ch) << endl;
 	}
+
+	ch->sendtochar(str);
+}
+
+void initcmddict(void)
+{
+	cmddict.insert(QString("quit"), new cmdentry_t("quit", cmd_quit));
+	cmddict.insert(QString("who"), new cmdentry_t("who", cmd_who));
+	initcommcmddict();
 }

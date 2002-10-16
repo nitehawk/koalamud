ee5133e15c8eeb8c67094eb872945d3a1cbb01f7
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-10-16 04:05:37 +0000

    (Logical change 1.9)

---
+++ koalamud/cmdtree.cpp
@@ -0,0 +1,190 @@
+/***************************************************************\
+*                        KoalaMud Gen 2                         *
+*    Copyright (c) 2002 First Step Internet Services, Inc.      *
+*                     All Rights Reserved                       *
+*        Distributed under the terms of the FSI License         *
+* See file LICENSE in the root of this package for information  *
+\***************************************************************/
+/***************************************************************\
+*	Module:
+*	Author:
+* Description:
+* Classes:
+\***************************************************************/
+
+#define KOALA_CMDTREE_CXX "%A%"
+
+#include <ctype.h>
+#include <qregexp.h>
+
+#include "cmdtree.hxx"
+
+namespace koalamud {
+
+/*******
+ * Stuff for the CommandTreeNodes
+ */
+
+/** Return a pointer to the command tree node for a given edge
+ * @param let Letter to return edge for.  This is lowercased and normalized as
+ * needed automatically.
+ * @return index into edges for the given char
+ */
+unsigned short CommandTree::CommandTreeNode::edgenum(char let)
+{
+	unsigned char lowerlet = tolower(let);
+	if (lowerlet < 'a' || lowerlet > 'z')
+		return numedges - 1;
+	else
+		return lowerlet - 'a';
+}
+
+/*******
+ * Stuff for the CommandTree
+ */
+
+/** Build a CommandTree object
+ * This constructor builds an empty CommandTree by creating a new root node
+ * and setting up all of our tracking variables
+ */
+CommandTree::CommandTree(void)
+{
+	rootnode = new CommandTreeNode("", NULL, 0);
+}
+
+/** Destroy CommandTree
+ * Run a depth first traversal of the entire CommandTree, deleting the nodes
+ * as we come upwards in the traversal
+ */
+CommandTree::~CommandTree(void)
+{
+	destroybranch(rootnode);
+	delete rootnode;
+}
+
+/** Destroy a branch of the CommandTree
+ * Recursively destroy a branch of a CommandTree
+ * @param branch Branch to destroy
+ */
+void CommandTree::destroybranch(CommandTreeNode *branch)
+{
+	for (unsigned int i=0; i<branch->numedges; i++)
+	{
+		if (branch->edges[i])
+		{
+			destroybranch(branch->edges[i]);
+			delete branch->edges[i];
+		}
+	}
+}
+
+/** Add a command to the tree
+ * Inject the given command into the tree, adjusting the position of existing
+ * nodes as needed to maintain the correct structure of the tree.
+ * @param name Command name
+ * @param fact Pointer to command factory
+ * @param id Command identifier to the factory
+ * @return true if command was successfully injected, false otherwise.  We
+ * return false if the command already exists in the command tree.
+ * @note The command tree is case insensitive.
+ * @todo We need to make sure that the shortest commands bubble to the top
+ */
+bool CommandTree::addcmd(QString name, CommandFactory *fact, unsigned int id)
+{
+	CommandTreeNode *newnode = new CommandTreeNode(name, fact, id);
+	unsigned int len = name.length();
+	CommandTreeNode *loc = rootnode;
+	const char *cname = name.latin1();
+
+	/* FIXME: We still need to handle non-alpha commands */
+	for (unsigned int i = 0; i< len; i++)
+	{
+		/* If the edge is null, we just put our new node into that spot */
+		if (loc->edges[loc->edgenum(cname[i])] == NULL)
+		{
+			loc->edges[loc->edgenum(cname[i])] = newnode;
+			newnode->parent = loc;
+			newnode->depth = i+1;
+			return true;
+		}
+		loc = loc->edges[loc->edgenum(cname[i])];
+	}
+
+	delete newnode;
+	return false;
+}
+
+/** Find a command without abbreviations.  If the strings do not completely
+ * match, we return NULL.
+ * @param cmd String to find
+ * @return Pointer to node with string or NULL if not found.
+ */
+CommandTree::CommandTreeNode *CommandTree::find_full(QString cmd)
+{
+	CommandTreeNode *loc = rootnode;
+	bool done = false;
+	unsigned short len;
+	const char *ccmd = cmd.latin1();
+
+	if ((len = cmd.length()) < 1)
+	{
+		return NULL;
+	}
+
+	/* FIXME: We need to handle non-alpha commands too */
+	do
+	{
+		/* If the strings match, we're done */
+		if (cmd == loc->_name)
+		{
+			return loc;
+		} else if (len == loc->depth) // we're at the end of cmd with no match
+			break;
+
+		/* If it doesn't match, we need to traverse down a level.  */
+		if ((loc = loc->edges[loc->edgenum(ccmd[loc->depth])]) == NULL)
+			break;
+	} while (!done);
+	
+	return NULL;
+}
+
+/** Find a command or an abbreviation of a command.
+ * We will always match the full command if possible.
+ * @param cmd Command to search for
+ * @return pointer to CommandTreeNode matching command
+ */
+CommandTree::CommandTreeNode *CommandTree::find_abbrev(QString cmd)
+{
+	CommandTreeNode *loc = rootnode;
+	bool done = false;
+	unsigned short len;
+	const char *ccmd = cmd.latin1();
+	QRegExp rx(cmd);
+
+	if ((len = cmd.length()) < 1)
+	{
+		return NULL;
+	}
+
+	/* FIXME: We need to handle non-alpha commands too */
+	do
+	{
+		/* If the strings match, we're done */
+		if (cmd == loc->_name)
+		{
+			return loc;
+		} else if (rx.search(loc->_name) == 0) { // abbreviation match
+			return loc;
+		} else if (len == loc->depth) // we're at the end of cmd with no match
+			break;
+
+		/* If it doesn't match, we need to traverse down a level.  */
+		if ((loc = loc->edges[loc->edgenum(ccmd[loc->depth])]) == NULL)
+			break;
+	} while (!done);
+	
+	return NULL;
+}
+
+}; /* end koalamud namespace */
---
+++ koalamud/cmdtree.hxx
@@ -0,0 +1,168 @@
+/***************************************************************\
+*                        KoalaMud Gen 2                         *
+*    Copyright (c) 2002 First Step Internet Services, Inc.      *
+*                     All Rights Reserved                       *
+*        Distributed under the terms of the FSI License         *
+* See file LICENSE in the root of this package for information  *
+\***************************************************************/
+/***************************************************************\
+*	Module: Command
+*	Author: Matthew Schlegel
+* Description: Provides the command tree classes
+* Classes:
+\***************************************************************/
+
+#ifndef KOALA_CMDTREE_HXX
+#define KOALA_CMDTREE_HXX "%A%"
+
+#include <zthread/FastRecursiveMutex.h>
+
+#include "main.hxx"
+#include "cmd.hxx"
+
+namespace koalamud {
+
+/** Defines a tree of commands
+ * This class defines the command tree used in command lookups.  For
+ * simplicity, we consolidate all of the command available type of checks to
+ * the commands themselves.
+ */
+class CommandTree {
+	public:
+		/** An individual node in the command tree */
+		class CommandTreeNode {
+			protected:
+				/** Pointer to the command factory */
+				CommandFactory *_factory;
+				/** Command Factory local ID */
+				unsigned int _fid;
+				/** Full name of this node */
+				QString _name;
+				/** Number of edges */
+				static const unsigned int numedges = 27;
+				/** Pointers to subordinate nodes - One for each letter + 1 for
+				 * non-alpha */
+				CommandTreeNode *edges[numedges];
+				/** Pointer to parent */
+				CommandTreeNode *parent;
+				/** Depth is equivalent to the position in the name string that this
+				 * node is using in the parents edges array.
+				 * depth==2 means that for 'hello', parent->edgefor('e') == this
+				 */
+				unsigned short depth;
+				
+			public:
+				/** Contructor takes command name, factory, and id
+				 * @param name Full command name
+				 * @param fact Pointer to factory
+				 * @param id Command ID
+				 */
+				CommandTreeNode(QString name, CommandFactory *fact, unsigned int id)
+					: _factory(fact), _fid(id), _name(name), parent(NULL), depth(0)
+					{ for (unsigned int i = 0; i < numedges; i++) { edges[i] = NULL; } }
+
+				unsigned short edgenum(char letter);
+
+				/** Instantiate the class
+				 * @param ch Pointer to character
+				 */
+				Command *createcommand(K_PlayerChar *ch)
+				{ if (_factory != NULL) return _factory->create(_fid, ch);
+					else return NULL; }
+
+			public:
+				/** Operator new overload */
+				void * operator new(size_t obj_size)
+					{ return koalamud::PoolAllocator::alloc(obj_size); }
+				/** Operator delete overload */
+				void operator delete(void *ptr)
+					{ koalamud::PoolAllocator::free(ptr); }
+
+				/** Make sure that CommandTree can manipulate our data (for traversal
+				 * and adding nodes */
+				friend class CommandTree;
+		}; /* End class CommandTreeNode */
+
+	public:
+		CommandTree(void);
+		~CommandTree(void);
+		/** Operator new overload */
+		void * operator new(size_t obj_size)
+			{ return koalamud::PoolAllocator::alloc(obj_size); }
+		/** Operator delete overload */
+		void operator delete(void *ptr)
+			{ koalamud::PoolAllocator::free(ptr); }
+
+		bool addcmd(QString name, CommandFactory *fact, unsigned int id);
+
+		CommandTreeNode *find_full(QString cmd);
+		CommandTreeNode *find_abbrev(QString cmd);
+
+		/** Find a command and create a command object
+		 * Search the command tree and create a command object based on what we
+		 * find.
+		 * @param cmd Command to find
+		 * @param ch Character pointer to pass on to command
+		 * @param abbrev true if we want to match abbreviations
+		 * @return pointer to new command object or null if it could not be found.
+		 */
+		Command *findandcreate(QString cmd, K_PlayerChar *ch, bool abbrev = false)
+		{
+			CommandTreeNode *cmdptr = NULL;
+			
+			if (abbrev)
+				cmdptr = find_abbrev(cmd);
+			else
+				cmdptr = find_full(cmd);
+
+			if (cmdptr)
+				return cmdptr->createcommand(ch);
+
+			return NULL;
+		}
+
+	protected:
+		/** Pointer to the root of the CommandTree */
+		CommandTreeNode *rootnode;
+		/** Lock for the tree to prevent corruption */
+		ZThread::FastRecursiveMutex treelock;
+
+		void destroybranch(CommandTreeNode *branch);
+};
+
+/** Singleton wrapper for the main command tree
+ * This class simply provides a wrapper around the main command tree that acts
+ * as a specialized smart pointer singleton
+ */
+class MainCommandTree
+{
+	public:
+		/** Detroy main command tree */
+		~MainCommandTree() { delete maincmdtree; }
+		/** Return a pointer to the main command tree */
+		CommandTree * operator->()
+		{
+			static bool initialized = false;
+			if (!initialized)
+			{
+				maincmdtree = new CommandTree;
+				initialized = true;
+			}
+			return maincmdtree;
+		}
+
+	protected:
+		/** Pointer to our command tree */
+		CommandTree *maincmdtree;
+};
+
+}; /* KoalaMud namespace */
+
+
+#ifdef KOALA_CMDTREE_CXX
+koalamud::MainCommandTree  maincmdtree;
+#else
+extern koalamud::MainCommandTree  maincmdtree;
+#endif
+
+#endif // KOALA_CMDTREE_HXX

868653b8f083a2afad0b91b9bb59dd8c569395f2
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-10-21 23:50:47 +0000

    convert main to koalamud::MainServer Add argument parsing Add daemonizing code
    (Logical change 1.11)

--- koalamud/main.cpp
+++ koalamud/main.cpp
@@ -16,54 +16,197 @@
 #include <qapplication.h>
 #include <qstatusbar.h>
 
+#include <getopt.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
 #include "main.hxx"
 #include "database.hxx"
 #include "network.hxx"
 #include "koalastatus.h"
 #include "cmdtree.hxx"
 
-/** Main entry point and server startup */
-int main( int argc, char **argv )
+namespace koalamud {
+
+/** Main server constructor
+ * Here we initialize and prepare the various subsystems and load all of the
+ * server configuration information from the database.
+ */
+MainServer::MainServer( int argc, char **argv )
+	: _executor(NULL), _guiactive(true), _background(false),
+		_profile("default")
 {
-	/* Seed the allocator with some pool sizes to get things going */
-	//koalamud::PoolAllocator::instance()->createpool(260);
+	/* Call to process arguments here */
+	if (!parseargs(argc, argv))
+		return;
 
-	//maincmdtree->addcmd("test", NULL, 0);
-	//maincmdtree->addcmd("test1", NULL, 0);
-	//maincmdtree->addcmd("test2", NULL, 0);
+	/* Daemonizing code here */
+	if (_background)
+		daemonize();
 
-	cout << *koalamud::PoolAllocator::instance() << endl;
+	/* create our application object */
+  _app = new QApplication( argc, argv, _guiactive );
 
-	executor = new ZThread::PoolExecutor<false>(TPMIN, TPMAX);
-  guiactive = true;
-
-  /* FIXME:  we need to scan argv for an argument to turn off the gui */
-  QApplication a( argc, argv, guiactive );
+	if (_guiactive) {
+     _statwin = new KoalaStatus();
+     _statwin->statusBar()->message("booting");
+     _statwin->show();
+     _app->setMainWidget(_statwin);
+   }
 
 	/* Start database */
-	if (!initdatabasesystem())
+	_kmdb = new koalamud::Database;
+	if (!_kmdb->isonline())
 	{
 		/* Houston, we have a problem! */
 		cerr << "FATAL:  Unable to connect to SQL server!" << endl;
-		executor->cancel();
-		executor->join();
-		return 1;
+		return;
 	}
 
-   if (guiactive) {
-     stat = new KoalaStatus();
-     stat->statusBar()->message("online");
-     stat->show();
-     a.setMainWidget(stat);
-   }
+	/* Initialize our task pool executor */
+	_executor = new ZThread::PoolExecutor<false>(threadpoolmin, threadpoolmax);
+}
+
+/** Start everything running */
+void MainServer::run(void)
+{
+	if (!_kmdb || !_kmdb->isonline())
+		return;
 
 	/* Start a listener */
+	/* FIXME:  This information should be retrieved from the database */
 	new KoalaServer(4444);
+	new KoalaServer(6464);
 
-  int res = a.exec();
-	executor->cancel();
-	executor->join();
+	/* Update status bar */
+	if (_guiactive) {
+     _statwin->statusBar()->message("online");
+   }
 
-	cout << *koalamud::PoolAllocator::instance() << endl;
-	return res;
+	_app->exec();
+}
+
+/** Shut down game server */
+MainServer::~MainServer(void)
+{
+	if (_executor) {
+		_executor->cancel();
+		_executor->join();
+	}
+
+	delete _kmdb;
+	delete _app;
+}
+
+/** Parse command line arguments */
+bool MainServer::parseargs(int argc, char **argv)
+{
+	char opt = EOF; // Current option
+
+	opterr = 0;
+
+	const char optlist[] = "hfbgGp:";
+
+	while ((opt = getopt(argc, argv, optlist)) != -1)
+	{
+		switch (opt)
+		{
+			case 'f':
+				_background = false;
+				break;
+			case 'b':
+				_background = true;
+				break;
+			case 'g':
+				_guiactive = false;
+				break;
+			case 'G':
+				_guiactive = true;
+				break;
+			case 'p':
+				_profile = optarg;
+				break;
+			case ':':
+				cout << "Missing argument to " << argv[optind] << endl;
+			default:
+			case '?':
+				cout << usage();
+				return false;
+		}
+	}
+
+	return true;
+}
+
+/** Return current version string */
+QString MainServer::versionstring(void)
+{
+	return "KoalaMud Gen2 v0.3.5a";
+}
+
+/** Return server usage string */
+QString MainServer::usage(void)
+{
+	QString outstr;
+	QTextOStream os(&outstr);
+	os << versionstring() << endl << endl <<
+"Usage:" << endl <<
+"  koalamud [-h]" << endl << endl << 
+"  -h         This help screen" << endl <<
+"  -f         Run server in foreground (default)" << endl <<
+"  -b         Run server in background" << endl <<
+"  -g         disable GUI" << endl <<
+"  -G         enable GUI (default)" << endl;
+
+	return outstr;
+}
+
+/** Drop server into background for execution */
+void MainServer::daemonize(void)
+{
+	int status = -1;
+
+	/* Stage 1, spawn into child process and close handles */
+	status = fork();
+	switch (status)
+	{
+		case -1:
+			perror("daemonize()::fork() - 1");
+			exit(errno);
+		case 0: // child
+			break;
+		default: // parent - exit
+			exit(0);
+	}
+
+	/* Switch process groups to release our parent */
+	status = setsid();
+	if (status == -1)
+	{
+		perror("daemonize()::setsid()");
+		exit(errno);
+	}
+
+	/* close handles */
+	fclose(stdin);
+	fclose(stdout);
+	fclose(stderr);
+
+}
+
+}; /* end koalamud namespace */
+
+/** C++ code entry point.
+ * Create our MainServer object and call into it to start the server running.
+ */
+int main( int argc, char **argv )
+{
+	srv = new koalamud::MainServer(argc,argv);
+
+	srv->run();
+
+	delete srv;
+
+	return 0;
 }

1b3a00688d26f810a8af2e46f6b6cf04e9cf07b4
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-10-31 07:32:53 +0000

    add PlayerCreationParser class.
    (Logical change 1.17)

--- koalamud/parser.hxx
+++ koalamud/parser.hxx
@@ -108,6 +108,60 @@ class PlayerParser : public Parser
 		virtual void parseLine(QString line);
 };
 
+/** Player Creation Parser
+ * This parser handles all of the new player creation parsing tasks, including
+ * getting player name, race, initial role and class, and stats information.
+ * This class also handles creating a basic database record for the new player
+ * and spawning a character object for them before switching to the
+ * PlayerParser.
+ */
+class PlayerCreationParser : public Parser
+{
+	public:
+		PlayerCreationParser(ParseDescriptor *desc, QString name=NULL);
+		/** Destroy a parser object */
+		virtual ~PlayerCreationParser(void) {}
+
+	public: /* virtual functions */
+		virtual void parseLine(QString line);
+
+	protected: /* Internal usage functions */
+		void createDBRecord(void);
+		/** Check to see if a name is allowed.
+		 * @bug Temporary implementation */
+		bool checkName(QString name)
+			{ if (!name.isEmpty())return true; return false;}
+		/** Check for a valid email address
+		 * @bug Temporary implementation */
+		bool checkEmail(QString email)
+			{ if (!email.isEmpty())return true; return false;}
+
+	protected: /* Various data gathered in creation process */
+		/** Character first name */
+		QString _fname;
+		/** Character last name */
+		QString _lname;
+		/** Character password */
+		QString _pass;
+		/** Email address - Used for updates/password reminders, etc. */
+		QString _email;
+
+		/** Current process state values */
+		typedef enum {
+			STATE_GETNAME, /**< Get a character name */
+			STATE_CONFNAME, /**< Confirm selected player name */
+			STATE_GETLAST, /**< Get character last name */
+			STATE_CONFLAST, /**< Confirm character last name */
+			STATE_GETPASS, /**< Get password */
+			STATE_CONFPASS, /**< Confirm password - they have to type it in again */
+			STATE_GETEMAIL, /**< Get an email address from them */
+			STATE_CONFEMAIL, /**< Confirm entered email */
+		} state_t;
+
+		/** Current parser state */
+		state_t curstate;
+};
+
 }; /* end koalamud namespace */
 
 #endif //  KOALA_PARSER_HXX

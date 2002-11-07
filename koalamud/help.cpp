65d24c07ceb0c459c28fca79c8b13f83301ca96b
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-11-07 05:33:23 +0000

    Add helpedit command Add HelpOLC class
    (Logical change 1.18)

--- koalamud/help.cpp
+++ koalamud/help.cpp
@@ -15,6 +15,7 @@
 #define KOALA_HELP_CXX "%A%"
 
 #include "help.hxx"
+#include "logging.hxx"
 
 namespace koalamud {
 	namespace commands {
@@ -164,6 +165,103 @@ class Help : public Command
 		}
 };
 
+/** Help Edit command class
+ */
+class HelpEdit : public Command
+{
+	public:
+		/** Pass through constructor */
+		HelpEdit(Char *ch) : Command(ch) {}
+		/** Run help edit command */
+		virtual unsigned int run(QString args)
+		{
+			QString str;
+			QTextOStream os(&str);
+			QString topic = args.section(' ', 0, 0);
+			int topnum = topic.toInt();
+			QSqlQuery q;
+
+			/* If we didn't get any arguments, set topnum to display the first help
+			 * topic in the database which should be an overview page
+			 * @todo Don't display help pages that _ch does not have access to. */
+			if (topic.isEmpty())
+			{
+				os << "You must specify a help entry to edit" << endl;
+				_ch->sendtochar(str);
+				return 1;
+			}
+
+			/* if our help topic is 'search' check for additional data in args,
+			 * otherwise simply look for topic 'search' as usual. */
+			if (topic.lower() == "new")
+			{
+				topnum = 0;
+			}
+
+			/* Do a normal help lookup.  If we get 1 result, display it.  If we get
+			 * more than one result, display a listing similar to our search
+			 * listing.  Otherwize say we couldn't find any matches */
+			{
+				QString query;
+				QTextOStream qos(&query);
+				qos << "Select helpid, title, body from helptext" << endl;
+				qos << "where keywords like '%" << args << "%';";
+				if (q.exec(query))
+				{
+					unsigned int total;
+					if ((total = q.numRowsAffected()) == 1)
+					{
+						q.next();
+						topnum = q.value(0).toInt();
+					} else if (total > 1) {
+						topnum = -1;
+						os << "Help topic search results matching: " << endl
+							 << "'" << args << "'" << endl;
+						os << "Topic Number      Title" << endl;
+						unsigned int count = 0;
+						while (q.next() && count < 18)
+						{
+							count++;
+							os.width(12);
+							os << q.value(0).toString();
+							os.width(0);
+							os << "  " << q.value(1).toString() << endl;
+						}
+
+						os << endl << "Displayed first " << count << " results of "
+							 << total << " results total." << endl;
+						_ch->sendtochar(str);
+						return 0;
+					}
+				}
+			}
+
+			if (topnum >=0)
+			{
+				Parser *_oldparser = _ch->getDesc()->parser();
+				_ch->getDesc()->setParser(
+							new HelpOLC(_ch, _ch->getDesc(), _oldparser, topnum), false);
+				return 0;
+			}
+
+			return 1;
+		}
+
+		/** Restricted access command. */
+		virtual bool isRestricted(void) const { return true;}
+
+		/** Command Groups */
+		virtual QStringList getCmdGroups(void) const
+		{
+			QStringList gl;
+			gl << "Implementor" << "Coder" << "Builder" << "HelpEditor";
+			return gl;
+		}
+
+		/** Get command name for individual granting */
+		virtual QString getCmdName(void) const { return QString("helpedit"); }
+};
+
 	}; /* end commands namespace */
 
 /** Command Factory for help.cpp */
@@ -175,6 +273,7 @@ class Help_CPP_CommandFactory : public CommandFactory
 			: CommandFactory()
 		{
 			maincmdtree->addcmd("help", this, 1);
+			maincmdtree->addcmd("helpedit", this, 2);
 		}
 
 		/** Handle command object creations */
@@ -184,6 +283,8 @@ class Help_CPP_CommandFactory : public CommandFactory
 			{
 				case 1:
 					return new koalamud::commands::Help(ch);
+				case 2:
+					return new koalamud::commands::HelpEdit(ch);
 			}
 			return NULL;
 		}
@@ -191,4 +292,98 @@ class Help_CPP_CommandFactory : public CommandFactory
 
 /** Command factory for help.cpp module.  */
 Help_CPP_CommandFactory Help_CPP_CommandFactoryInstance;
+
+/** Start editing a help entry */
+HelpOLC::HelpOLC(Char *ch, ParseDescriptor *pd, Parser *oldParser,
+						long entry = 0)
+	: olc(ch, pd, oldParser), _entry(entry)
+{
+	/* Add fields */
+	addField(QString("Help ID"), FIELD_INTEGER, &_entry, false);
+	addField(QString("Title"), FIELD_STRING, &_topic, true, 80);
+	addField(QString("Keywords"), FIELD_STRING, &_keywords, true, 255);
+	addField(QString("Body"), FIELD_MULTILINE, &_body, true);
+
+	/* Load entry */
+	if (entry > 0)
+	{
+		if (load())
+		{
+			sendMenu();
+			return;
+		}
+		_entry = 0;
+	}
+	_topic = "New Topic";
+	_keywords = "New Keywords";
+	_body = "New Help Entry";
+	sendMenu();
+}
+
+/** Load help entry from database for editing. */
+bool HelpOLC::load(void)
+{
+	QString query;
+	QTextOStream qos(&query);
+	QSqlQuery q;
+
+	qos << "select helpid, title, keywords, body from helptext " << endl
+			<< "where helpid = " << _entry << ";";
+	if (q.exec(query) && q.numRowsAffected())
+	{
+		q.next();
+		_entry = q.value(0).toUInt();
+		_topic = q.value(1).toString();
+		_keywords = q.value(2).toString();
+		_body = q.value(3).toString();
+		return true;
+	}
+
+	return false;
+}
+
+/** Save help entry to database */
+void HelpOLC::save(void)
+{
+	QString query;
+	QTextOStream qos(&query);
+	QSqlQuery q;
+
+	if (_entry == 0)
+	{
+		qos << "insert into helptext (title, keywords, body) values " << endl
+				<< "('" << _topic << "', '" << _keywords << "', '" << _body << "');";
+	}
+	else
+	{
+		qos << "update helptext " << endl
+				<< "set title='" << _topic << "', " << endl
+				<< "keywords='" << _keywords << "', " << endl
+				<< "body='" << escapeString(_body) << "'" << endl
+				<< "where helpid = " << _entry << ";";
+	}
+	if (q.exec(query))
+	{
+		if (_entry == 0)
+		{
+			/* Doesn't look like we can get the last insert ID, so we'll query for a
+			 * specific match to our title and keywords */
+			QString q2;
+			QTextOStream os2(&q2);
+			os2 << "select helpid from helptext where " << endl
+					<< "title = '" << _topic << "' and keywords = '" << _keywords
+					<< "';";
+			if (q.exec(q2) && q.next())
+			{
+				_entry = q.value(0).toUInt();
+			}
+		}
+	} else {
+		QString out;
+		QTextOStream os(&out);
+		os << "Failed save query in help editor: " << query;
+		Logger::msg(out, Logger::LOG_ERROR);
+	}
+}
+
 }; /* end koalamud namespace */
--- koalamud/help.hxx
+++ koalamud/help.hxx
@@ -17,6 +17,34 @@
 
 #include "cmdtree.hxx"
 #include "cmd.hxx"
+#include "olc.hxx"
 
+namespace koalamud {
+
+/** Help OLC module
+ * This Parser/OLC class handles editing help entries
+ */
+class HelpOLC : public olc
+{
+	public:
+		HelpOLC(Char *ch, ParseDescriptor *pd, Parser *oldParser,
+						long entry = 0);
+
+	public:
+		virtual void save(void);
+		virtual bool load(void);
+	
+	protected:
+		/** Help entry database ID */
+		long int _entry;
+		/** Help Entry Topic */
+		QString _topic;
+		/** Help Entry Keywords */
+		QString _keywords;
+		/** Help Entry Text */
+		QString _body;
+};
+	
+}; /* end koalamud namespace */
 
 #endif //  KOALA_HELP_HXX

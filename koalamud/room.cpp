828c0aa9b9162dd01aa1abc2a336fd2bd1e5f5a4
goldbuilders.com!nitehawk <goldbuilders.com!nitehawk>
2002-11-10 22:16:28 +0000

    Completely new room code - *very* basic, but now working
    (Logical change 1.23)

--- koalamud/room.cpp
+++ koalamud/room.cpp
@@ -14,10 +14,13 @@
 
 #define KOALA_ROOM_CXX "%A%"
 
-#include "room.hxx"
+#include <qregexp.h>
 
-/* {{{ K_Room Implementation */
-/* {{{ Constructors */
+#include "room.hxx"
+#include "logging.hxx"
+
+namespace koalamud {
+
 /** Load a room by its coordinate reference.
  * This is the standard constructor.  The default values will create a new
  * room for OLC, Any other coordinates load the room from the database or find
@@ -27,27 +30,15 @@
  * @param ele Room elevation coordinate
  * @param zone Zone of the room, only used for new rooms
  */
-K_Room::K_Room(int lat=defnewlat, int longi=defnewlong, int ele=defnewele,
-								unsigned int zone=1)
-	: QObject(NULL, NULL), KRefObj(), _virtual(false),
-		_lat(lat), _long(longi), _elev(ele),
-		_zoneid(zone), _indatabase(false)
+Room::Room(int zone, int lat, int longi, int ele)
+	: _virtual(false), _zone(zone), _lat(lat), _long(longi), _elev(ele)
 {
-	if (lat == defnewlat && longi == defnewlong && ele == defnewele)
-	{
-		/* Construction is simple, just fill in default values for everything and
-		 * return */
-		_plrlimit = 0;
-		_lvlmin = _lvlmax = 0;
-		_title = "A new room";
-		_description = "You are in a new room without a description.";
-		_rtype = TYPE_CITY;
-		_flags = FLAG_NOFLAGS;
-	} else
-	{
-		/* We need to load the room from the database or construct the room from
-		 * the wilderness areas */
-	}
+	for (int d = DIR_NORTH; d <= DIR_DOWN; d++)
+		exits[d] = NULL;
+
+	load();
+
+	RoomMap.insert(getRef(zone, lat, longi, ele), this);
 }
 
 /** Virtual Room (not saved) - used for generating mazes and other dynamic
@@ -56,31 +47,161 @@ K_Room::K_Room(int lat=defnewlat, int longi=defnewlong, int ele=defnewele,
  * various temporary usages.  These rooms might be used for generating dynamic
  * mazes and perhaps some forms of transportation.
  */
-K_Room::K_Room(QString title, QString desc, flags_t flags=FLAG_NOFLAGS, type_t rtype=TYPE_INDOORS)
-	: QObject(NULL, NULL), KRefObj(), _title(title), _description(desc),
-		_virtual(false), _flags(flags), _rtype(rtype), _zoneid(1),
-		_indatabase(false)
+Room::Room(QString title, QString desc, QString smell, QString sound,
+						QString soundfile,
+						int zone, int lat, int longi, int elev,
+						flags_t flags=FLAG_NOFLAGS, type_t rtype=TYPE_INDOORS,
+						unsigned int plrlimit=0)
+	: _title(title), _description(desc), _smell(smell), _sound(sound),
+		_soundfile(soundfile), _virtual(true),
+		_zone(zone), _lat(lat), _long(longi), _elev(elev),
+		_plrlimit(plrlimit), _flags(flags), _rtype(rtype)
 {
+	for (int d = DIR_NORTH; d <= DIR_DOWN; d++)
+		exits[d] = NULL;
+
+	RoomMap.insert(getRef(zone, lat, longi, elev), this);
 }
 
 /** Destructor - Save room if needed and free memory */
-K_Room::~K_Room()
+Room::~Room()
 {
+	RoomMap.remove(getRef(_zone, _lat, _long, _elev));
 }
-/* }}} Constructors */
 
-/* {{{ Database Fuctions */
-/** Actual database load, will also handle generating wilderness rooms */
-bool K_Room::load()
+/** Load a room from the database
+ * @note This is only for non-virtual rooms
+ */
+bool Room::load()
 {
-	return false;
+	if (_virtual)
+		return false;
+
+	QString query;
+	QTextOStream qos(&query);
+	QSqlQuery q;
+
+	qos << "select title, description, flags, type, plrlimit" << endl
+			<< "from room where zone = " << _zone << endl
+			<< "and latitude = " << _lat << " and longitude = " << _long << endl
+			<< "and elevation = " << _elev << ";";
+	if (q.exec(query) && q.numRowsAffected())
+	{
+		q.next();
+		_title = q.value(0).toString();
+		_description = q.value(1).toString();
+		_plrlimit = q.value(4).toUInt();
+		return true;
+		/* Flags and type need to be interpreted */
+	} else {
+		QString log;
+		QTextOStream out(&log);
+		out << "Problem loading room " << getRef(_zone, _lat, _long, _elev);
+		Logger::msg(log, Logger::LOG_ERROR);
+		return false;
+	}
+
 }
 
 /** Save room to the database */
-bool K_Room::save()
+bool Room::save()
 {
 	return false;
 }
 
-/* }}} Database Fuctions */
-/* }}} K_Room Implementation */
+/** Display room to a character
+ * This should check light levels, Character visibility and all the other
+ * related things while it is building the display string */
+QString Room::displayRoom(Char *ch, bool brief=false)
+{
+	QString disp;
+	QTextOStream out(&disp);
+	
+	out << "|B-== |Y" << _title << " |B==-|x" << endl;
+	
+	if (!brief)
+	{
+		out << endl << _description << endl;
+	}
+
+	/* Display character short descriptions */
+	QPtrListIterator<Char> curchar(charsinroom);
+	for (; *curchar; ++curchar)
+	{
+		if (*curchar == ch)
+			continue;
+
+		out << (*curchar)->getShortDesc(ch) << endl;
+	}
+
+	/* List objects in room */
+	
+	return disp;
+}
+
+/** Send a message to everyone in the room */
+void Room::sendToRoom(Char *from, Char *to, QString msg, QString fromtmpl,
+				QString totmpl, QString roomtmpl)
+{
+	QPtrListIterator<Char> ch(charsinroom);
+	QString out;
+
+	for (; *ch; ++ch)
+	{
+		if (*ch == from)
+		{
+			out = fromtmpl.replace(QRegExp("%sender%"), "You");
+			if (to)
+				out = out.replace(QRegExp("%victim%"), to->getName(*ch));
+			else
+				out = out.replace(QRegExp("%victim%"), "");
+		} else if (*ch == to)
+		{
+			out = totmpl.replace(QRegExp("%victim%"), "You");
+			if (from)
+				out = out.replace(QRegExp("%sender%"), from->getName(*ch));
+			else
+				out = out.replace(QRegExp("%sender%"), "");
+		} else
+		{
+			if (from)
+				out = roomtmpl.replace(QRegExp("%sender%"), from->getName(*ch));
+			else
+				out = roomtmpl.replace(QRegExp("%sender%"), "");
+			if (to)
+				out = out.replace(QRegExp("%victim%"), to->getName(*ch));
+			else
+				out = out.replace(QRegExp("%victim%"), "");
+		}
+
+		out = out.replace(QRegExp("%message%"), msg);
+
+		(*ch)->sendtochar(out);
+
+		if (*ch && *ch != from)
+		{
+			(*ch)->sendPrompt();
+		}
+	}
+}
+
+/** Change the coordinates of a room
+ * This is used primarily in the Wilderness generation code
+ */
+void Room::moveRoom(int zone, int lat, int longi, int elev)
+{
+	RoomMap.remove(getRef(_zone, _lat, _long, _elev));
+	_zone = zone;
+	_lat = lat;
+	_long = longi;
+	_elev = elev;
+	RoomMap.insert(getRef(zone, lat, longi, elev), this);
+}
+
+/** Find a room in the Room Map */
+Room *Room::findRoom(int zone, int lat, int longi, int elev)
+{
+	return RoomMap[getRef(zone, lat, longi, elev)];
+}
+
+}; /* end koalamud namespace */
--- koalamud/room.hxx
+++ koalamud/room.hxx
@@ -15,9 +15,93 @@
 #ifndef KOALA_ROOM_HXX
 #define KOALA_ROOM_HXX "%A%"
 
-#include <qobject.h>
+#include <qstring.h>
+#include <qdict.h>
+#include <bitset>
 
-#include "autoptr.hxx"
+#include <zthread/FastRecursiveMutex.h>
+
+#include "memory.hxx"
+
+/* Predefine classes */
+namespace koalamud {
+class Room;
+};
+
+#include "char.hxx"
+
+namespace koalamud {
+
+/** Room Exit
+ * This provides the connection between rooms.  This is a one way connection
+ * *only*.  Two way connections use an exit each direction.
+ * @note The Room class is responsible for saving exits to the database where
+ * needed.  This saves us the burden of needing virtual flags and similar.
+ */
+class RoomExit
+{
+	public:
+		/** Flags for this exit */
+		typedef enum {
+			/** No flags - direct link */
+			FLAG_NONE = 0,
+			/** Exit is a door */
+			FLAG_DOOR,
+			/** Door is lockable */
+			FLAG_LOCKABLE,
+			/** Door is locked */
+			FLAG_LOCKED,
+			/** Door is lockpick proof */
+			FLAG_PICKPROOF,
+			/** Door is closed */
+			FLAG_CLOSED,
+			/** Door is magically locked */
+			FLAG_MAGICLOCK,
+			/** Door/Exit is hidden */
+			FLAG_HIDDEN,
+			/** Exit is trapped - search source room for affects */
+			FLAG_TRAPPED,
+			/** Lock is magic proof (no magicunlock) */
+			FLAG_MAGICPROOF,
+			/** Door is bash proof - no muscling our way in */
+			FLAG_BASHPROOF,
+			/** This must be the last flag */
+			FLAG_ENDLIST
+		} flag_t;
+		
+	public:
+		/** Initialize a room exit */
+		RoomExit(Room *destroom, flag_t exitflags, QString name=NULL,
+							unsigned int keyobj=0)
+			: _name(name), dest(destroom), flags(exitflags), keynum(keyobj)
+		{}
+		
+	public: /* Operators */
+		/** Operator new overload */
+		void * operator new(size_t obj_size)
+			{ return koalamud::PoolAllocator::alloc(obj_size); }
+		/** Operator delete overload */
+		void operator delete(void *ptr)
+			{ koalamud::PoolAllocator::free(ptr); }
+
+	public:
+		/** Is the exit visible */
+		bool isVisible(void) const { return (!(flags[FLAG_HIDDEN]));}
+		/** Return true if a flag is set */
+		bool isSet(flag_t flag) const { return (flags[flag]);}
+		/** Return pointer to destination room or NULL if the door is closed */
+		Room *getDest(void) { if (flags[FLAG_CLOSED]) return NULL; return dest;}
+		/** Toggle flag and return new state */
+		bool toggleFlag(flag_t flag)
+			{ flags[flag] = !flags[flag];
+				return flags[flag];}
+
+	protected:
+		QString _name; /**< Exit name (used in the case of doors */
+		Room *dest; /**< Other end of this link */
+		bitset<FLAG_ENDLIST> flags; /**< Exit flags */
+		unsigned int keynum; /**< Object ID that is key */
+};
 
 /** KoalaMud Room.
  *
@@ -25,57 +109,42 @@
  *
  * Representation of an in game room.
  *
- * @warning Still a lot to be cleaned up here, both in implementation and
- * design.  A lot of work will be needed to ensure that wilderness rooms in
- * particular do not end up consuming all available memory.
+ * This may be subclassed to make some usages easier as well.
  */
-class K_Room : public QObject, public virtual KRefObj
+class Room
 {
-	Q_OBJECT
-
 	public:
-		/** Constant default latitude used to identify requests for a new OLC
-		 * room. */
-		static const int defnewlat = 2000000;
-		/** Constant default longitude used to identify requests for a new OLC
-		 * room. */
-		static const int defnewlong = 2000000;
-		/** Constant default elevation used to identify requests for a new OLC
-		 * room. */
-		static const int defnewele = 2000000;
-		/** Smart Pointer type */
-		typedef KRefPtr<K_Room> autoptr;
 		/** Room Flags.
-		 * The list of all flags possible to be set on a room */
+		 * The list of all flags possible to be set on a room
+		 * This should match the order used in the database */
 		typedef enum {
 				/** No flags set */
-				FLAG_NOFLAGS = 0x0,
+				FLAG_NOFLAGS = 0,
 				/** Room has a level restriction, can be minimum or maximum or both.*/
-				FLAG_LEVELRESTRICT = 0x1,
+				FLAG_LEVELRESTRICT,
 				/** Room provides fast regen. */
-				FLAG_REGEN = 0x2,
+				FLAG_REGEN,
 				/** Room is dark - outdoor rooms are dark at night. */
-				FLAG_DARK = 0x4,
+				FLAG_DARK,
 				/** Room is a deathtrap. */
-				FLAG_DEATHTRAP = 0x8,
+				FLAG_DEATHTRAP,
 				/** No NPC's allowed in room. */
-				FLAG_NONPC = 0x10,
+				FLAG_NONPC,
 				/** Room is a safe spot, no fighting. */
-				FLAG_SAFE = 0x11,
+				FLAG_SAFE,
 				/** Room is a save spot.  Players not logged in for an extended
 				 * period are drawn to savespot rooms */
-				FLAG_SAVESPOT = 0x12,
+				FLAG_SAVESPOT,
 				/** Room is a recall location. */
-				FLAG_RECALLSPOT = 0x14,
+				FLAG_RECALLSPOT,
 				/** Track command is blocked at this room. */
-				FLAG_NOTRACK = 0x18,
+				FLAG_NOTRACK,
 				/** No magic allowed in this room. */
-				FLAG_NOMAGIC = 0x20,
+				FLAG_NOMAGIC,
 				/** Teleport command can choose this room as a target. */
-				FLAG_TELEPORT = 0x21,
-				/** Room is part of the wilderness.  Changes saving semantics and some
-				 * aspects of room loading and lifetimes. */
-				FLAG_WILDERNESS = 0x22
+				FLAG_TELEPORT,
+				/** Last flag - used for sizing bitset */
+				FLAG_ENDLIST
 				} flags_t;
 		/** Room Types
 		 * @notes Generally speaking, Room types INDOORS, COVERED, FIELD, and CITY
@@ -83,51 +152,106 @@ class K_Room : public QObject, public virtual KRefObj
 		 * be found in wilderness areas.  
 		 * @todo  Add Road type(s)
 		 */
-		typedef enum {TYPE_INDOORS = 0, /**< Room in Inside of a building */
-									TYPE_COVERED,	/**< Room is Covered in some form */
-									TYPE_FIELD, /**< Room is a field - perhaps a park */
-									TYPE_CITY, /**< Room is a city, but not one of the above */
-									TYPE_FARM, /**< Room is part of a farm */
-									TYPE_FOREST, /**< Room is part of a forest */
-									TYPE_HILL,	/**< Room is part of a hill */
-									TYPE_MOUNTAIN, /**< Room is part of a mountain */
-									TYPE_WATER, /**< Room is at the surface of water */
-									TYPE_UNDERWATER, /**<< Room is underwater */
-									TYPE_FLYING /**< Room is in the air */
-									} type_t;
+		typedef enum {
+			TYPE_INDOORS = 0, /**< Room in Inside of a building */
+			TYPE_COVERED,	/**< Room is Covered in some form */
+			TYPE_FIELD, /**< Room is a field - perhaps a park */
+			TYPE_CITY, /**< Room is a city, but not one of the above */
+			TYPE_FARM, /**< Room is part of a farm */
+			TYPE_FOREST, /**< Room is part of a forest */
+			TYPE_HILL,	/**< Room is part of a hill */
+			TYPE_MOUNTAIN, /**< Room is part of a mountain */
+			TYPE_WATER, /**< Room is at the surface of water */
+			TYPE_UNDERWATER, /**<< Room is underwater */
+			TYPE_FLYING /**< Room is in the air */
+			} type_t;
+
+		/** Map directions to their position in the exit pointer array
+		 * @note DIR_DOWN must be the last element in the list */
+		typedef enum {
+			DIR_NORTH = 0,
+			DIR_NORTHEAST,
+			DIR_EAST,
+			DIR_SOUTHEAST,
+			DIR_SOUTH,
+			DIR_SOUTHWEST,
+			DIR_WEST,
+			DIR_NORTHWEST,
+			DIR_UP,
+			DIR_DOWN,
+		} directions;
 
 	public:
-		K_Room(int lat=defnewlat, int longi=defnewlong, int ele=defnewele,
-						unsigned int zone=1);
-		K_Room(QString, QString, flags_t rflags=FLAG_NOFLAGS,
-						type_t rtype=TYPE_INDOORS);
-		virtual ~K_Room();
+		Room(int zone, int lat, int longi, int ele);
+		Room(QString, QString, QString, QString, QString,
+						int, int, int, int,
+						flags_t rflags=FLAG_NOFLAGS, type_t rtype=TYPE_INDOORS,
+						unsigned int plrlimit=0);
+		virtual ~Room();
 
 		virtual bool load(void);
 		virtual bool save(void);
 
+	public: /* Operators */
+		/** Operator new overload */
+		void * operator new(size_t obj_size)
+			{ return koalamud::PoolAllocator::alloc(obj_size); }
+		/** Operator delete overload */
+		void operator delete(void *ptr)
+			{ koalamud::PoolAllocator::free(ptr); }
+
+	public:
+		/** Call to add a character to a room */
+		void enterRoom(Char *ch)
+			{ lock.acquire();
+				charsinroom.append(ch);
+				lock.release(); }
+		/** Call to remove a character from a room */
+		void leaveRoom(Char *ch)
+			{ lock.acquire();
+				charsinroom.remove(ch);
+				lock.release(); }
+		void moveRoom(int, int, int, int);
+		QString displayRoom(Char *ch, bool brief=false);
+		void sendToRoom(Char *from, Char *to, QString msg, QString fromtmpl,
+						QString totmpl, QString roomtmpl);
+
+	public:  /* Static functions */
+		static Room *findRoom(int zone, int lat, int longi, int elev);
+		/** Return a string representing a room location */
+		static QString getRef(int zone, int lat, int longi, int elev)
+			{ QString ref; QTextOStream os(&ref);
+				os << "(" << zone << "," << lat << "," << longi << "," << elev << ")";
+				return ref; }
+
 	protected:
 		QString _title; /**< Room Title */
 		QString _description;  /**<  Room description */
+		QString _smell; /**< Smell description */
+		QString _sound; /**< Sound description */
+		QString _soundfile; /**< filename for sound using mud sound protocol */
 		bool _virtual;	/**< Flag to denote a virtual, unsaved room */
+		int _zone; /**< Zone Coordinate */
 		int _lat; /**< Room latitude coordinate */
 		int _long; /**< Room longitude coordinate */
 		int _elev; /**< Room Elevation coordinate */
 		/** Maximum number of players in room, 0 is unlimited */
 		unsigned int _plrlimit;
-		unsigned int _lvlmin; /**< Minimum level to enter, 0 is no minimum */
-		unsigned int _lvlmax; /**< Maximum level to enter, 0 is no minimum */
-		flags_t _flags;  /**< Room flags */
+		bitset<FLAG_ENDLIST> _flags;  /**< Room flags */
 		type_t _rtype;  /**< Room type */
-		/** Zone identifier, used for various purposes.
-		 * @todo Possibly add a pointer to the zone object and remove this version
-		 * in favor of the pointer.
-		 */
-		unsigned int _zoneid;
-		/** Flag to change the behaviour of save to handle a room already in the
-		 * database. */
-		bool _indatabase;
-
+		ZThread::FastRecursiveMutex lock; /**< Lock for room */
+		/** Pointers to exit objects to connect to neighbor rooms */
+		RoomExit *exits[DIR_DOWN+1];
+		/** List of chars in room */
+		QPtrList<Char> charsinroom;
 };
+
+#ifdef KOALA_ROOM_CXX
+QDict<Room> RoomMap(8388607);
+#else
+extern QDict<Room> RoomMap;
+#endif
+
+}; /* end koalamud namespace */
 
 #endif //  KOALA_ROOM_HXX
